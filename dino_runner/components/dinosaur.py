cb3ff99fd63f4c44b33894ab6b79e61dfa2a9520
Joan-Enrique-Nieto-Reina <joanenriquen@gmail.com>
2023-07-14 15:26:29 -0500

    Create dinosaur.py

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,14 @@
+# Crear la clase dinosaur. 
+# add_ init_ .
+# addd loop metodos (update, draw) pass.
+import pygame
+
+class dinosaur:
+    def __init__ (self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
