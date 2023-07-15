895b570032c9ec7ea2fdc09471802901e999a083
Joan-Enrique-Nieto-Reina <joanenriquen@gmail.com>
2023-07-14 20:37:01 -0500

    class two

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,14 +1,79 @@
+# Clase Nº 1.
 # Crear la clase dinosaur. 
 # add_ init_ .
 # addd loop metodos (update, draw) pass.
+from pygame._sprite import sprite
+from dino_runner.utils.constants import (
+    RUNNING,
+    DUCKING,
+    JUMPING 
+)
+
 import pygame
 
-class dinosaur:
+class Dinosaur(sprite):
+
+    POS_x = 80
+    POS_Y = 300
+    DUCK_POS_Y = 340
+    JUMP_VEL = 8.5
+
     def __init__ (self):
-        pass
+        self.image = RUNNING(0)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.POS_x
+        self.rect.y = self.POS_Y
+        self.step_index = 0
+        self.running = True
+        self.ducking = False
+        self.jumping = False    
+        self.jumping_velocity = self.JUMP_VEL
 
-    def update(self):
-        pass
+    def update(self, user_input):
+        if self.jumping:
+            self.jump()
+        if self.running:
+            self.run()
+        if self.ducking:
+            self.duck()
+        if user_input(pygame.K_DOWN) or user_input(pygame.Ks) and not self.jumping:
+            self.running = False
+            self.ducking = True
+            self.jumping = False
+        elif user_input(pygame.K_UP):
+            self.running = False
+            self.ducking = False
+            self.jumping = True
+        elif not self.jumping:
+            self.running = True
+            self.ducking = False
+            self.jumping = False
+        if self.step_index >= 10: 
+            self.step_index = 0
 
-    def draw(self):
-        pass
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def run(self):
+        self.image = RUNNING(0) if self.step_index < 5 else RUNNING(1) #Cambio de imagne 1 a 2.
+        self.rect = self.image.get_rect()
+        self.rect.x = self.POS_x
+        self.rect.y = self.POS_Y
+        self.step_index += 1    
+
+    def jump(self):
+        self.image = JUMPING
+        if self.jumping:
+            self.rect.y -= self.jumping_velocity * 4
+            self.jumping_velocity -= 0.8 # Baja la velocidad del dino.
+        if self.jumping_velocity < -self.JUMP_VEL:
+            self.rect.y = self.POS_Y
+            self.jumping = False
+            self.jumping_velocity = self.JUMP_VEL
+
+    def duck(self):
+        self.image = DUCKING(0) if self.step_index < 5 else DUCKING(1) #Cambio de imagne 1 a 2.
+        self.rect = self.image.get_rect()
+        self.rect.x = self.POS_x
+        self.rect.y = self.DUCK_POS_Y
+        self.step_index += 1    
--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from dino_runner.components.game import Game
 if __name__ == "__main__":
     game = Game()
     game.run()
-# == tarea clase 1
+# == tarea clase 1.
 # herencia en programacion 
 # sprite
 # pygame rect
+
+# tarea clas 2.
+# agrgar elementos al bacgraund.
+# cambiar la skin del dinosaurio.
+
